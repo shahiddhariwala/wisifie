241c405670954a8e4d1a5551c2dc84d7fe1c3b54
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-10-20 01:10:10 +0530

    WIP: Section added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      backgroundImage: {
+        "hero-pattern": "url('./assets/spiral-bg-1.svg')",
+      },
       colors: {
         "section-red": "#753032",
         "text-pink": "#B47C7B",
