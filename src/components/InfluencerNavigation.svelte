12fb3e8e817ee54c68860ffe9b9889d8454a1932
Shahid Dhariwala <shahid.dhariwala@gmail.com>
2023-10-31 23:20:53 +0530

    Svelte addition

---
+++ src/components/InfluencerNavigation.svelte
@@ -0,0 +1,17 @@
+<script>
+  import Modal from "./Modal.svelte";
+
+  let showModal = false;
+</script>
+
+<button on:click={() => (showModal = true)}> show modal </button>
+
+<Modal bind:showModal>
+  <!-- <InformationLayout title="Infleuncers">
+        {sortedInfluencers.map((each, index) => <InfluencerProfileChip image_url={each?.data?.image_url} 
+        name={each?.data?.name} 
+        index={index} 
+        id={each?.data?.social_media_handle}/>)}
+    </InformationLayout> -->
+  Shahid Dhariwala Rocks
+</Modal>
